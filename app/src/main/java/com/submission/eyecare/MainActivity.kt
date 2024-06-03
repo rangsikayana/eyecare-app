d4dee615fc3898c49e3f667532d73c12a6f5293d
Fadilano <fadilanoa@gmail.com>
2024-06-03 11:36:39 +0700

    add new app icon

--- app/src/main/java/com/submission/eyecare/MainActivity.kt
+++ app/src/main/java/com/submission/eyecare/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.submission.eyecare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.submission.eyecare.databinding.ActivityMainBinding
+import com.submission.eyecare.ui.LoginActivity
+import com.submission.eyecare.ui.RegisterActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+// Sementara
+        binding.login.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+         binding.register.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+// Sementara
+
     }
 }
